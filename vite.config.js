43806f8e95581e16f2fa371cc9007d237b62c26e
wein1m <weynn.sz@gmail.com>
2026-06-08 22:05:33 +0800

    add offline PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: ["icon.png"],
+
+      manifest: {
+        name: "Laporan PKL Yodha",
+        short_name: "PKL Yodha",
+        description: "Interactive Internship Presentation",
+        theme_color: "#f9f2e9",
+
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpeg,svg,otf,ttf}"],
+      },
+    }),
+  ],
 });
